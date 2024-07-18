be356b7129a3e4c337e7ddc432d528012a486653
Marlon Angeli <iam@marlonangeli.com.br>
2024-07-18 16:42:26 +0000

    change modpack source to CurseForge

--- clean.sh
+++ clean.sh
@@ -8,13 +8,16 @@ docker_down() {
     docker compose down
 }
 
+docker_rm_volume() {
+    docker volume rm minecraft-data
+}
+
 # Are you sure?
 echo "Are you sure you want to remove all files and stop the server? (y/n)"
 read -r response
 if [ $response == "y" ]; then
     docker_down
-    remove_files "data/*"
-    remove_files "logs/*"
+    docker_rm_volume
     echo "All files have been removed and the server has been stopped."
 else
     echo "No files have been removed."
